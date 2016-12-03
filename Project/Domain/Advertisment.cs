f4bd75a5129e8c9c34c692dca20b054342cc14f6
JackyJack64791 <aleksei.kosenko.64791@gmail.com>
2016-12-03 23:59:34 +0700

    Создан проект с реализованными сущностями

---
+++ Project/Domain/Advertisment.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Advertisment: EntityBase
+    {
+        enum Type { Performer, Customer }
+
+        [Required]
+        public Type TypeOfAuthor { get; set; }
+        [Required]
+        public User Author { get; set; }
+        [Required]
+        public Category ServiceCategory { get; set; }
+        [Required]
+        public string Text { get; set; }
+        public float Cost { get; set; }
+        public List<Feedback> HistoryOfFeedbacks { get; set; }
+    }
+}
