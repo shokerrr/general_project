7ea120dd05a28a2a42327074f3e851bbbdf7b689
JackyJack64791 <aleksei.kosenko.64791@gmail.com>
2016-12-04 17:40:57 +0700

    Базовый интерфейс

---
+++ Project/Repository/IRepositoryBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Repository
+{
+    public interface IRepositoryBase<T> where T:EntityBase
+    {
+        IQueryable<T> GetAll();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        void Delete(int id);
+    }
+}
